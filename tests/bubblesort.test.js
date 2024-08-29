8bf18f1d5560157319d996293d156de802cceb78
Bsakwa <sakwabrian23@gmail.com>
2024-08-29 11:06:32 +0300

    Added mocha tests for the bubble sort algorithm::

---
+++ tests/bubblesort.test.js
@@ -0,0 +1,34 @@
+import { expect } from 'chai';
+import BubbleSort from '../searches/bubblesort.js';
+
+describe('BubbleSort Class', () => {
+    it('should sort an array of numbers in ascending order', () => {
+        const sorter = new BubbleSort([5, 3, 8, 4, 2]);
+        const sortedArray = sorter.sort();
+        expect(sortedArray).to.deep.equal([2, 3, 4, 5, 8]);
+    });
+
+    it('should handle an empty array', () => {
+        const sorter = new BubbleSort([]);
+        const sortedArray = sorter.sort();
+        expect(sortedArray).to.deep.equal([]);
+    });
+
+    it('should handle an array with one element', () => {
+        const sorter = new BubbleSort([1]);
+        const sortedArray = sorter.sort();
+        expect(sortedArray).to.deep.equal([1]);
+    });
+
+    it('should handle an array that is already sorted', () => {
+        const sorter = new BubbleSort([1, 2, 3, 4, 5]);
+        const sortedArray = sorter.sort();
+        expect(sortedArray).to.deep.equal([1, 2, 3, 4, 5]);
+    });
+
+    it('should handle an array with duplicate elements', () => {
+        const sorter = new BubbleSort([3, 3, 2, 1, 3]);
+        const sortedArray = sorter.sort();
+        expect(sortedArray).to.deep.equal([1, 2, 3, 3, 3]);
+    });
+});
